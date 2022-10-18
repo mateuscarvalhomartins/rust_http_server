d2175948292e6b91243cff7649e63c2442135b68
mateuscarvalhomartins <81715392+mateuscarvalhomartins@users.noreply.github.com>
2022-10-18 00:07:26 -0300

    replace placeholder files

---
+++ public/index.js
@@ -0,0 +1,51 @@
+let n1 = '0', n2 = '0', res, op = ''
+
+const operations = {
+  '+': () => Number.parseInt(n1) + Number.parseInt(n2),
+  '-': () => Number.parseInt(n1) - Number.parseInt(n2),
+  '/': () => Number.parseInt(n1) / Number.parseInt(n2),
+  '*': () => Number.parseInt(n1) * Number.parseInt(n2),
+}
+
+function registerNumber(n) {
+  if(!op) {
+    if(n != 0 || n1 != '0') {
+      if(n1 == '0') n1 = ''
+      n1 += n
+      document.querySelector('textarea').innerText = n1
+    }
+  } else {
+    if(n != 0 || n2 != '0') {
+      if(n2 == '0') n2 = ''
+      n2 += n
+      document.querySelector('textarea').innerText = n2
+    }
+  }
+}
+
+function registerOperator(aop) {
+  op = aop
+  document.querySelector('textarea').innerText = op
+}
+
+function clearLast() {
+  if(!op) {
+    n1 = '0'
+    document.querySelector('textarea').innerText = n1
+  } else {
+    n2 = '0'
+    document.querySelector('textarea').innerText = n2
+  }
+}
+
+function clearAll() {
+  n1 = '0', n2 = '0', op = ''
+  document.querySelector('textarea').innerText = '0'
+}
+
+function result() {
+  res = operations[op]()
+  clearAll()
+  n1 = res.toString()
+  document.querySelector('textarea').innerText = res
+}
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ use std::thread;
 use std::collections::HashMap;
 
 fn main() {
-  let server = HttpServer::new();
-  server.listen(8080, |stream, mut server| {
-    server.add_static_files("./public/", "");
+  let mut server = HttpServer::new();
+  
+  server.add_static_files("./public/", "");
 
+  server.listen(8080, |stream, server| {
     thread::spawn(move || {
       let request = server.read_request(&stream);
       let response = server.send_response(&stream, &request, HashMap::new());
+      
       println!("Request:\n{}\nResponse:\n{}", request.to_string(), response.to_string());
     });
   })
